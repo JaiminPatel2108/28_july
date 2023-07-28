97edd6e9e64db4807aa9939df45e714a34002fb5
Jaimin <pjaimind031@gmail.com>
2023-07-28 12:08:38 +0530

    this is my first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express()
+const port = process.env.PORT || 2000
+
+app.get("/",(req,res)=>{
+    res.send("hello from the jaimin")
+})
+
+app.listen(port,()=>{
+    console.log(`your backend server is running on port ${port}`);
+})
